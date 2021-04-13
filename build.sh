2c1a70641a88618409f808aa5dbd4e5e12041a54
100Daisy <48625180+100Daisy@users.noreply.github.com>
2021-04-13 22:08:05 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,13 +11,14 @@ git config --global color.ui false
 # Clone device tree and common tree
 git clone https://github.com/100Daisy/AnyKernel3
 git clone https://github.com/100Daisy/android_kernel_motorola_deen -b 11 StockPlus
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 gcc-4.9
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 gcc
 
 ls -l
 echo $PWD
+export PATH="/home/builder/Kramel/gcc/bin:$PATH"
+
 # Build Kernel
 cd StockPlus
-export PATH="~/Kramel/gcc-4.9/bin:$PATH"
 make deen_defconfig ARCH=arm64 O=out
 make -j$(nproc --all) O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-
 
