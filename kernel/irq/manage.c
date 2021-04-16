5e4a7a3a36eb6db1514b6493e0435d1af27ef9ce
100Daisy <100Daisy@protonmail.com>
2021-04-16 12:31:00 +0200

    fixes

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -315,7 +315,7 @@ irq_set_affinity_notifier(unsigned int irq, struct irq_affinity_notify *notify)
 		cancel_work_sync(&old_notify->work);
 
 	if (old_notify) {
-		if (cancel_work_sync(&old_notify->work));
+		if (cancel_work_sync(&old_notify->work)) {
 			/* Pending work had a ref, put that one too */
 			kref_put(&old_notify->kref, old_notify->release);
 		}
